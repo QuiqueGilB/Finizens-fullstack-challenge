95bea45f7287ed65060a6fbc38efc4f94292a4c6
Enrique Gil <user@enriquegil.me>
2021-02-28 21:48:29 +0100

    refactor construct

--- src/SharedContext/SharedModule/Domain/Exception/FinizensException.php
+++ src/SharedContext/SharedModule/Domain/Exception/FinizensException.php
@@ -7,10 +7,5 @@ use Throwable;
 
 abstract class FinizensException extends Exception
 {
-    public function __construct($message = "", Throwable $previous = null)
-    {
-        parent::__construct($message, static::errorCode(), $previous);
-    }
-
     abstract protected static function errorCode(): string;
 }
