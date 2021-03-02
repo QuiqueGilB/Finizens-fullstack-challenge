036e60f081e37ea0ed074d21ba4f7237f51c4d30
Enrique Gil <user@enriquegil.me>
2021-03-02 21:34:17 +0100

    not found response resolvable

--- src/SharedContext/HttpModule/Infrastructure/Listener/MakeResponseOnExceptionListener.php
+++ src/SharedContext/HttpModule/Infrastructure/Listener/MakeResponseOnExceptionListener.php
@@ -2,9 +2,13 @@
 
 namespace FinizensChallenge\SharedContext\HttpModule\Infrastructure\Listener;
 
+use FinizensChallenge\InvestmentContext\PortfolioModule\Domain\Exception\PortfolioNotFoundException;
+use FinizensChallenge\InvestmentContext\SharedModule\Domain\Exception\SharedPortfolioNotFoundException;
 use FinizensChallenge\SharedContext\HttpModule\Infrastructure\Response\EmptyResponse;
 use FinizensChallenge\SharedContext\HttpModule\Infrastructure\Response\InvalidPayloadResponse;
+use FinizensChallenge\SharedContext\HttpModule\Infrastructure\Response\NotFoundResponse;
 use FinizensChallenge\SharedContext\SharedModule\Domain\Exception\ValidationException;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
@@ -13,12 +17,14 @@ class MakeResponseOnExceptionListener
     public function onKernelException(ExceptionEvent $event): void
     {
         // TODO: Create ExceptionResolverService
-        if ($event->getThrowable() instanceof MethodNotAllowedHttpException) {
-            $event->setResponse(new EmptyResponse( 405));
-        }
 
-        if ($event->getThrowable() instanceof ValidationException) {
-            $event->setResponse(new InvalidPayloadResponse());
-        }
+        $response = match (get_class($event->getThrowable())) {
+            MethodNotAllowedHttpException::class => new EmptyResponse(Response::HTTP_METHOD_NOT_ALLOWED),
+            ValidationException::class => new InvalidPayloadResponse(),
+            PortfolioNotFoundException::class,
+            SharedPortfolioNotFoundException::class => new NotFoundResponse()
+        };
+
+        $event->setResponse($response);
     }
 }
