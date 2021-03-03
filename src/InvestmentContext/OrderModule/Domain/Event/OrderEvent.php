e7684d92cb0e2e3ef11c816dd7510188af8e9a97
Enrique Gil <user@enriquegil.me>
2021-03-03 22:10:32 +0100

    events payload

--- src/InvestmentContext/OrderModule/Domain/Event/OrderEvent.php
+++ src/InvestmentContext/OrderModule/Domain/Event/OrderEvent.php
@@ -9,7 +9,16 @@ abstract class OrderEvent extends Event
 {
     public function __construct(Order $order)
     {
-        parent::__construct($order);
+        parent::__construct([
+            'id' => $order->id()->value(),
+            'portfolioId' => $order->allocationId()->value(),
+            'allocationId' => $order->allocationId()->value(),
+            'shares' => $order->shares()->value(),
+            'orderType' => $order->orderType()->value(),
+            'orderStatus' => $order->orderStatus()->value(),
+            'updatedAt' => $order->createdAt()->format(DATE_ATOM),
+            'createdAt' => $order->createdAt()->format(DATE_ATOM)
+        ]);
     }
 
     protected static function type(): string
